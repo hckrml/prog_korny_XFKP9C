a35a84e38e564358bc6b8f74aaa9913e3db488d8
hckrml <84583535+hckrml@users.noreply.github.com>
2022-01-12 19:20:45 +0100

    Add files via upload

---
+++ Egyszam.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace XFKP9C
+{
+    class Egyszam
+    {
+        private string nev;
+
+        List<int> fordulok = new List<int>();
+
+        public Egyszam(string sor)
+        {
+            string[] d = sor.Split(' ');
+            nev = d[0];
+            for(int i = 1; i < d.Length; i++)
+            {
+                fordulok.Add(Convert.ToInt32(d[i]));
+            }
+        }
+
+        public string Nev { get => nev; set => nev = value; }
+
+        public List<int> Fordulok { get => fordulok; set => fordulok = value; }
+    }
+}
---
+++ Form1.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace XFKP9C
+{
+    public partial class Form1 : Form
+    {
+        static List<Egyszam> egyszamLista = new List<Egyszam>();
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("egyszamjatek1.txt", Encoding.UTF8);
+            string sor = "";
+            while (!sr.EndOfStream)
+            {
+                sor = sr.ReadLine();
+                Egyszam esz = new Egyszam(sor);
+                egyszamLista.Add(esz);
+            }
+            sr.Close();
+        }
+
+        private void txTippek_TextChanged(object sender, EventArgs e)
+        {
+            string tippek = txTippek.Text;
+            string[] d = tippek.Split(' ');
+            int tippSzam = d.Length;
+            label3.Text = tippSzam + " db";
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string nev = txNev.Text;
+            bool vanIlyen = false;
+            int index = 0;
+            while(vanIlyen == false && index < egyszamLista.Count)
+            {
+                if(egyszamLista[index].Nev == nev)
+                {
+                    vanIlyen = true;
+                }
+                else
+                {
+                    index++;
+                }
+            }
+
+            if (vanIlyen == true)
+            {
+                MessageBox.Show("Már van ilyen játékos!");
+            }
+            else
+            {
+                if(txTippek.Text.Split(' ').Length == 4)
+                {
+                    Egyszam esz = new Egyszam(nev + " " + txTippek.Text);
+                    egyszamLista.Add(esz);
+                    MessageBox.Show(egyszamLista.Count.ToString());
+
+                    FileStream fs = new FileStream("egyszamjatek1.txt", FileMode.Create);
+                    StreamWriter sw = new StreamWriter(fs);
+
+                    for(int i=0;i<egyszamLista.Count;i++)
+                    {
+                        sw.Write(egyszamLista[i].Nev + " ");
+                        sw.Write(egyszamLista[i].Fordulok[0] + " ");
+                        sw.Write(egyszamLista[i].Fordulok[1] + " ");
+                        sw.Write(egyszamLista[i].Fordulok[2] + " ");
+                        sw.WriteLine(egyszamLista[i].Fordulok[3]);
+                    }
+                    sw.Close();
+                    fs.Close();
+                    MessageBox.Show("Az adatokat kiírtam!");
+                }
+                else
+                {
+                    MessageBox.Show("Tippek száma nem megfelelő!");
+                }
+            }
+        }
+    }
+}
